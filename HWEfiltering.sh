9eb5eee1928a3b31564f342f9cd05f6ea024e22e
tjamann <tjamann@illinois.edu>
2016-10-21 09:06:38 -0500

    working on HWEfiltering sh script to put everything together

--- HWEfiltering.sh
+++ HWEfiltering.sh
@@ -1,2 +1,20 @@
+#!bin/bash
+set -e
+set -u
+
+date
+
+KEEP=keep_seq.txt
+
+#create the file with the 
 vcftools --gzvcf final.GATK.break.vcf.recode.vcf.gz --hardy
 python filterphetexcess.py
+vcftools --gzvcf allsamplesafterfiltering.mpileup.vcf.gz --keep $KEEP --remove-filtered-all --recode --out mpileup.filteredindiv
+vcftools --gzvcf final.GATK.break.vcf.recode.vcf.gz --keep $KEEP --remove-filtered-all --recode --out GATK.filteredindiv
+bgzip mpileup.filteredindiv.recode.vcf
+bgzip GATK.filteredindiv.recode.vcf
+tabix -p vcf mpileup.filteredindiv.recode.vcf.gz
+tabix -p vcf GATK.filteredindiv.recode.vcf.gz
+vcf-merge mpileup.filteredindiv.recode.vcf.gz GATK.filteredindiv.recode.vcf.gz final.hapmap.recode.vcf.gz > mpile.GATK.true.vcf
+vcftools --vcf mpile.GATK.true.vcf --012
+#then run r script het.RMD
